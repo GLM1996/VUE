e760173568ef0eff08489c3ca67fadc2e1e9ed1a
Gustavo <trabajoglm2021@gmail.com>
2024-10-17 00:13:19 -0400

    componente contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Hola</h1>
+    <button @click="incrementar">
+      Incrementar
+    </button>
+    <button @click="decrementar">
+      Decrementar
+    </button>
+    <p>{{ count }}</p>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const count = ref(0)
+  
+  const incrementar = () =>{
+    count.value++
+  }
+  const decrementar = () =>{
+    count.value--
+  }
+  
+  </script>
+  
+  <style scoped>
+    button{
+      background-color: blue;
+      color: white;
+      margin-left: 10px;
+      cursor: pointer;
+    }
+  </style>
+  
